536789aa30fdd2ff1dea60954040612242e9ab7e
ctrotsky <ctrotter136@gmail.com>
2016-11-02 13:49:28 -0700

    Began work on User/Group Observer/Subject relationship
    Users each have a UserView GUI object that they can show or hide.

---
+++ Twitter/src/twitter/Subject.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Colin
+ */
+public abstract class Subject {
+    
+    private List<Observer> observers = new ArrayList();
+    
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+    
+    public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+    
+    protected void notifyObservers(){
+        for (Observer observer : observers){
+            observer.update(this);
+        }
+    }
+    
+}
--- Twitter/src/twitter/User.java
+++ Twitter/src/twitter/User.java
@@ -9,6 +9,50 @@ package twitter;
  *
  * @author Colin
  */
-class User {
-    //extends node?
+class User extends Subject implements Observer{
+    private String uniqueID;
+    private String latestTweet;
+    private String newsFeed;
+    
+    private UserView userView;
+
+    
+    public User(String uniqueID){
+        
+    }
+    
+    public String getUniqueID(){
+        return uniqueID;
+    }
+    
+    public String getLatestTweet(){
+        return latestTweet;
+    }
+
+    @Override
+    public void update(Subject subject) {
+        if (subject instanceof User) {
+            postToNewsFeed(((User) subject).getUniqueID() + ": " + ((User) subject).getLatestTweet());
+        }
+    }
+    
+    public void postToNewsFeed(String tweet){
+        newsFeed = newsFeed + tweet + "\n";
+    }
+    
+    public void postTweet(String tweet){
+        //User should follow themself too so they can see their own tweets
+        latestTweet = tweet;
+        notifyObservers();
+    }
+    
+    public void openUserView(){
+        userView.setVisible(true);
+    }
+    
+    
+    
+    
+    
+    
 }
